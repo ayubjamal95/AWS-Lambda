129c9e2f260935e776b1ad6aba080551eef310f4
Ayub Jamal <ayubjamal95@gmail.com>
2022-11-18 00:31:53 +0500

    Update constants.ts

--- hello-world/constants.ts
+++ hello-world/constants.ts
@@ -1,6 +1,6 @@
 export class AppSettings
 {
-    static readonly signingKey= '1fcef2d99a03f5d47e2d3c551bee803e-b0ed5083-a0e7566f';
+    static readonly signingKey= 'XXXXXXX';
     static readonly bucketName ='ayubs3bucket';
     static readonly Provider = 'Mailgun';
     
